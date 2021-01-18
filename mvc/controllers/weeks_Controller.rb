2aa99febe7be98a43d9440971f4d2b99bf9763c8
amcmullen9291 <amcmullen9291@gmail.com>
2021-01-17 22:49:46 -0500

    index.erb summary sentence

--- mvc/controllers/weeks_Controller.rb
+++ mvc/controllers/weeks_Controller.rb
@@ -14,24 +14,25 @@ class WeeksController < EmployeesController
         @timecard[:badge_id] = @session[:badge_id]
         @timecard[:dept_id] = @session[:dept_id]
         @timecard[:monday_in] = @session[:monday_in]
-        @timecard[:monday_out] = @session[:monday_in]
-        @timecard[:tuesday_in] = @session[:monday_in]
-        @timecard[:tuesday_out] = @session[:monday_in]
-        @timecard[:wednesday_in] = @session[:monday_in]
-        @timecard[:wednesday_out] = @session[:monday_in]
-        @timecard[:thursday_in] = @session[:monday_in]
-        @timecard[:thursday_out] = @session[:monday_in]
-        @timecard[:friday_in] = @session[:monday_in]
-        @timecard[:friday_out] = @session[:monday_in]
-        @timecard[:saturday_in] = @session[:monday_in]
-        @timecard[:saturday_out] = @session[:monday_in]
-        @timecard[:sunday_in] = @session[:monday_in]
-        @timecard[:sunday_out] = @session[:monday_in]
+        @timecard[:monday_out] = @session[:monday_out]
+        @timecard[:tuesday_in] = @session[:tuesday_in]
+        @timecard[:tuesday_out] = @session[:tuesday_out]
+        @timecard[:wednesday_in] = @session[:wednesday_in]
+        @timecard[:wednesday_out] = @session[:wednesday_out]
+        @timecard[:thursday_in] = @session[:thursday_in]
+        @timecard[:thursday_out] = @session[:thursday_out]
+        @timecard[:friday_in] = @session[:friday_in]
+        @timecard[:friday_out] = @session[:friday_out]
+        @timecard[:saturday_in] = @session[:saturday_in]
+        @timecard[:saturday_out] = @session[:saturday_out]
+        @timecard[:sunday_in] = @session[:sunday_in]
+        @timecard[:sunday_out] = @session[:sunday_out]
+        @session[:week_ending] = params[:week][:week_ending]
         @session[:week_id] = @timecard[:id]
         @timecard.save  #current "week's Hour values already stored in @sessions
 
         @weeks = Week.select {|week, v| week[:badge_id] == @session[:employee_id]}
-        puts @weeks.first
+
         flash[:notice] = "Time Card Submitted. Thank You." 
         erb :"profile/weeks/index", :layout => false
     end
